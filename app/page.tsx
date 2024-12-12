48024dc49a987a54216f5664942080df880a9759
rehanmirza76 <abdulrehan0317@gmail.com>
2024-12-12 08:14:09 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -38,8 +38,7 @@ export default function Home() {
       <OurBlogs/>
       {/* ---------- INSTAGRAM SECTION -------------- */}
       <OurInsta/>
-      {/* ---------- Footer SECTION -------------- */}
-      <Footer/>
+   
     </>
   );
 }
