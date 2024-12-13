30dcca17de63bb6418f88f39a698bbf1df91b9e8
rehanmirza76 <abdulrehan0317@gmail.com>
2024-12-13 19:50:55 +0500

    update

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -112,15 +112,35 @@ export default function page() {
                 <p>: &nbsp;&nbsp;Sofa,Chair,Home,Shop</p>
                 <p className="flex items-center gap-3 text-black">
                   :&nbsp;&nbsp;
-                  <AiFillTwitterCircle  className="text-xl"/>
-                  <FaFacebook  className="text-xl"/>
-                  <FaLinkedin  className="text-xl"/>
+                  <AiFillTwitterCircle className="text-xl" />
+                  <FaFacebook className="text-xl" />
+                  <FaLinkedin className="text-xl" />
                 </p>
               </div>
             </div>
           </div>
         </div>
         <hr />
+        <div className="p-7 text-lg w-full flex flex-col justify-center items-center text-gray-400">
+          <div className="top flex justify-center gap-5">
+            <span className="text-black">Description</span>
+            <span>Additional Information</span>
+            <span>Reviews {"[5]"}</span>
+          </div>
+          <div className="flex flex-col gap-4 text-sm w-[85%] py-4">
+            <p>Embodying the raw, wayward spirit of rock n roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.</p>
+            <p>Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.</p>
+          </div>
+          <div className="flex gap-4">
+            <div className="bg-[#FFF9E5] py-10 p-4 flex items-center justify-center">
+              <img className="w-[30rem]" src="/sofa-1.png" alt="" />
+            </div>
+            <div className="bg-[#FFF9E5] py-10 p-4 flex items-center justify-center">
+              <img className="w-[30rem]" src="/sofa-2.png" alt="" />
+            </div>
+          </div>
+        </div>
+          <hr />
       </div>
     </div>
   );
